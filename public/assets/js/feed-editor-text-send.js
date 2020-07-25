c2a6ab54f8ff6860ccb7d63a6bbbe7cae3ba9e63
nikolasnwv <68400904+nikolasnwv@users.noreply.github.com>
2020-07-25 14:58:56 -0300

    definitivo o pc agora

---
+++ public/assets/js/feed-editor-text-send.js
@@ -0,0 +1,15 @@
+let feedInput = document.querySelector('.feed-new-input');
+let feedSubmit = document.querySelector('.feed-new-send');
+let feedForm = document.querySelector('.feed-new-form');
+
+if (feedSubmit.addEventListener('click', function(obj) {
+
+    feedSubmit.addEventListener('click', function(obj) {
+        let value = feedInput.innerText.trim();
+        
+        if (value != '') {
+            feedForm.querySelector('input[name=feededitortextsend]').value = value;
+            feedForm.submit();
+        }
+    });
+}));
